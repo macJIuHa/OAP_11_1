b79d2a0722b7df024794437a8d7a3b37a78daa3f
macJIuHa <ikruzov@bk.ru>
2021-02-25 16:29:42 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def about_me(firstname, surname, group):
+    print('Привет.')
+    print(f"Меня зовут {firstname} {surname}")
+    print(f"Я студент группы {group}")
+
+
+if __name__ == '__main__':
+    about_me(firstname="Антон",  surname="Чернов", group="РКТ2-71")
+
